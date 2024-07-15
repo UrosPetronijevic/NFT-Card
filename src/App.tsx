a9e0b6a1c9925b2191b0ea3ed581d7031e97531d
UrosPetronijevic <ukipetlja@gmail.com>
2024-07-15 16:48:55 +0200

    added the card structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
 export default function App() {
   return (
-    <div className=" bg-mainBg h-screen flex justify-center items-center"></div>
+    <div className=" bg-mainBg h-screen flex justify-center items-center">
+      <div className=" bg-cardBg h-3/5 w-1/5 flex flex-col">
+        <img />
+        <h1></h1>
+        <p></p>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
   );
 }
