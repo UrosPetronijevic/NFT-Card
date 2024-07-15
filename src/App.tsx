03b29908cd6c4b8157b360c5c7215fb05c4b37f4
UrosPetronijevic <ukipetlja@gmail.com>
2024-07-15 16:47:00 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+export default function App() {
+  return (
+    <div className=" bg-mainBg h-screen flex justify-center items-center"></div>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        mainBg: "hsl(217, 54%, 11%)",
+        cardBg: "hsl(216, 50%, 16%)",
+        line: "hsl(215, 32%, 27%)",
+        softBlue: "hsl(215, 51%, 70%)",
+      },
+    },
+  },
+  plugins: [],
+};
